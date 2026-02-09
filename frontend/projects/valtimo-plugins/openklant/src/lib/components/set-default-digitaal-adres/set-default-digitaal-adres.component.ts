c0fb9460c0f7a5da4006dfa1a86e0d51d70153e2
Jasper Kunkeler <jasper@risa-it.nl>
2026-02-09 13:13:14 +0100

    Continuation of component.ts, need to do handleValue

--- frontend/projects/valtimo-plugins/openklant/src/lib/components/set-default-digitaal-adres/set-default-digitaal-adres.component.ts
+++ frontend/projects/valtimo-plugins/openklant/src/lib/components/set-default-digitaal-adres/set-default-digitaal-adres.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, OnDestroy, OnInit, Output} from '@angular/core';
+import {FunctionConfigurationComponent, FunctionConfigurationData, PluginConfigurationData} from "@valtimo/plugin";
+import {BehaviorSubject, combineLatest, Observable, Subscription, take} from "rxjs";
+import {SetDefaultDigitaalAdresConfig} from "../../models/set-default-digitaal-adres-config";
 
 @Component({
   selector: 'lib-set-default-digitaal-adres',
   imports: [],
+  standalone: true,
   templateUrl: './set-default-digitaal-adres.component.html'
 })
-export class SetDefaultDigitaalAdresComponent {
+export class SetDefaultDigitaalAdresComponent
+  implements FunctionConfigurationComponent, OnInit, OnDestroy {
+  @Input() save$: Observable<void>;
+  @Input() disabled$: Observable<boolean>;
+  @Input() pluginId: string;
+  @Input() prefillConfiguration$?: Observable<SetDefaultDigitaalAdresConfig>;
+  @Output() valid: EventEmitter<boolean>;
+  @Output() configuration: EventEmitter<FunctionConfigurationData> =
+    new EventEmitter<FunctionConfigurationData>();
 
+  private saveSubscription!: Subscription;
+
+  private readonly formValue$ =
+    new BehaviorSubject<SetDefaultDigitaalAdresConfig | null>(null);
+  private readonly valid$ = new BehaviorSubject<boolean>(false);
+
+  ngOnInit(): void {
+    this.openSaveSubscription()
+  }
+
+  ngOnDestroy(): void {
+    this.saveSubscription?.unsubscribe();
+  }
+
+  formValueChange(formValue: SetDefaultDigitaalAdresConfig): void {
+    this.formValue$.next(formValue);
+    this.handleValid(formValue);
+  }
+
+  private handleValid(formValue: SetDefaultDigitaalAdresConfig): void {
+    const valid =
+  }
+
+  private openSaveSubscription(): void {
+    this.saveSubscription = this.save$?.subscribe((save) => {
+      combineLatest([this.formValue$, this.valid$])
+        .pipe(take(1))
+        .subscribe(([formValue, valid]) => {
+          if (valid && formValue) {
+            this.configuration.emit(formValue);
+          }
+        });
+    });
+  }
 }
