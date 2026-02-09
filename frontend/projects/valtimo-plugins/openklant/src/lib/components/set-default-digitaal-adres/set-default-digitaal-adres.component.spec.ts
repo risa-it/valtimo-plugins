f795cd7e49285100d1336198a2e003fff7c5bde3
Jasper Kunkeler <jasper@risa-it.nl>
2026-02-09 16:02:55 +0100

    Implementing tests

--- frontend/projects/valtimo-plugins/openklant/src/lib/components/set-default-digitaal-adres/set-default-digitaal-adres.component.spec.ts
+++ frontend/projects/valtimo-plugins/openklant/src/lib/components/set-default-digitaal-adres/set-default-digitaal-adres.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SetDefaultDigitaalAdresComponent } from './set-default-digitaal-adres.component';
+import {GetOrCreatePartijComponent} from "../get-or-create-partij/get-or-create-partij.component";
+import {EMPTY, of} from "rxjs";
+import {GetOrCreatePartijConfig} from "../../models/get-or-create-partij-config";
+import {SetDefaultDigitaalAdresConfig} from "../../models/set-default-digitaal-adres-config";
 
 describe('SetDefaultDigitaalAdresComponent', () => {
   let component: SetDefaultDigitaalAdresComponent;
@@ -10,10 +14,26 @@ describe('SetDefaultDigitaalAdresComponent', () => {
     await TestBed.configureTestingModule({
       imports: [SetDefaultDigitaalAdresComponent]
     })
-    .compileComponents();
+      .overrideTemplate(SetDefaultDigitaalAdresComponent, "")
+      .compileComponents();
+  });
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(SetDefaultDigitaalAdresComponent);
     component = fixture.componentInstance;
+
+    component.save$ = EMPTY;
+    component.disabled$ = of(false);
+    component.pluginId = "test-plugin";
+
+    component.prefillConfiguration$ = of({
+      resultPvName: "",
+      partijUuid: "",
+      adres: "",
+      soortDigitaalAdres: "",
+      verificatieDatum: "",
+    } as SetDefaultDigitaalAdresConfig);
+
     fixture.detectChanges();
   });
 
