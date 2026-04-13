0bf1477941d78b251f2cb141b56010fe4f805ce8
Jasper Kunkeler <jasper@risa-it.nl>
2026-04-13 11:54:01 +0200

    Remove Model and DTO

--- backend/samenwerkfunctionaliteit/src/main/kotlin/com/ritense/valtimoplugins/samenwerkfunctionaliteit/model/PartijInformation.kt
+++
@@ -1,9 +0,0 @@
-package com.ritense.valtimoplugins.samenwerkfunctionaliteit.model
-
-interface PartijInformation {
-    val bsn: String
-    val voorletters: String
-    val voornaam: String
-    val voorvoegselAchternaam: String
-    val achternaam: String
-}
