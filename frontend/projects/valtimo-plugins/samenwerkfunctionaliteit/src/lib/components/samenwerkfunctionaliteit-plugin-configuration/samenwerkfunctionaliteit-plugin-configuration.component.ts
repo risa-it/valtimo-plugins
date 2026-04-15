7bf333098e9f4adcddbd84c98fb7de199cd7a491
Jasper Kunkeler <jasper@risa-it.nl>
2026-04-15 14:42:06 +0200

    config component ts

--- frontend/projects/valtimo-plugins/samenwerkfunctionaliteit/src/lib/components/samenwerkfunctionaliteit-plugin-configuration/samenwerkfunctionaliteit-plugin-configuration.component.ts
+++ frontend/projects/valtimo-plugins/samenwerkfunctionaliteit/src/lib/components/samenwerkfunctionaliteit-plugin-configuration/samenwerkfunctionaliteit-plugin-configuration.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, OnDestroy, OnInit, Output} from '@angular/core';
+import {PluginConfigurationComponent} from '@valtimo/plugin';
+import {BehaviorSubject, combineLatest, Observable, Subscription, take} from 'rxjs';
+import {SamenwerkfunctionaliteitConfig} from '../../models/samenwerkfunctionaliteitConfig';
 
 @Component({
   selector: 'app-samenwerkfunctionaliteit-plugin-configuration',
@@ -6,6 +9,44 @@ import { Component } from '@angular/core';
   templateUrl: './samenwerkfunctionaliteit-plugin-configuration.component.html',
   styleUrl: './samenwerkfunctionaliteit-plugin-configuration.component.scss'
 })
-export class SamenwerkfunctionaliteitPluginConfigurationComponent {
+export class SamenwerkfunctionaliteitPluginConfigurationComponent implements PluginConfigurationComponent, OnInit, OnDestroy {
+  @Input() save$: Observable<void>;
+  @Input() disabled$: Observable<boolean>;
+  @Input() pluginId: string;
+  @Input() prefillConfiguration$?: Observable<SamenwerkfunctionaliteitConfig>;
 
+  @Output() valid: EventEmitter<boolean> = new EventEmitter<boolean>();
+  @Output() configuration: EventEmitter<SamenwerkfunctionaliteitConfig> = new EventEmitter<SamenwerkfunctionaliteitConfig>();
+
+  private saveSubscription: Subscription;
+
+  private readonly formValue$ = new BehaviorSubject<SamenwerkfunctionaliteitConfig | null>(null);
+  private readonly valid$ = new BehaviorSubject<boolean>(false);
+
+  ngOnInit(): void {
+    this.openSaveSubscription();
+  }
+
+  ngOnDestroy(): void {
+    this.saveSubscription?.unsubscribe();
+  }
+
+  formValueChange(formValue: SamenwerkfunctionaliteitConfig): void {
+    const valid = !!(formValue.samenwerkfunctionaliteitUrl && formValue.certificate);
+
+    this.valid$.next(valid);
+    this.valid.emit(valid);
+  }
+
+  private openSaveSubscription() {
+    this.saveSubscription = this.save$?.subscribe(() => {
+      combineLatest([this.formValue$, this.valid$])
+        .pipe(take(1))
+        .subscribe(([formValue, valid]) => {
+          if (valid) {
+            this.configuration.emit(formValue);
+          }
+        });
+    });
+  }
 }
