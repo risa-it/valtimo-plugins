51c38d27e2296151ae7b90ff9f7071e58466ef37
Jasper Kunkeler <jasper@risa-it.nl>
2026-04-15 11:27:33 +0200

    Cleared translations

--- frontend/projects/valtimo-plugins/samenwerkfunctionaliteit/src/lib/translations/nl.ts
+++ frontend/projects/valtimo-plugins/samenwerkfunctionaliteit/src/lib/translations/nl.ts
@@ -1,30 +1,4 @@
-
 export const pluginNlTranslations = {
-    openKlant: {
-        contactHistory: {
-            tabTitle: "Contactgeschiedenis",
-            loadingSpinner: {
-                loading: "Verversen...",
-                retrieved: "Opgehaald"
-            },
-            outcome: {
-                success: "Succes",
-                failure: "Mislukt",
-                notApplicable: "Niet van toepassing",
-                unknown: "Onbekend"
-            },
-            messages: {
-                noProperty: {
-                    title: "Geen contactgeschiedenis property",
-                    description: "Er bestaat geen 'contactgeschiedenis'-property in de dossierdefinitie. Voeg deze toe aan het dossierdefinitie."
-                },
-                emptyHistory: {
-                    title: "Geen contactgeschiedenis",
-                    description: "Er bestaat nog geen contactgeschiedenis voor dit dossier."
-                }
-            }
-        }
-    }
-
+  samenwerkfunctionaliteit: {}
 };
 
