dbcc9b0dde59c1dbfd3c4aad9cbedf3aa6f42748
Jasper Kunkeler <jasper@risa-it.nl>
2026-04-16 15:54:44 +0200

    Remove unnecesairy bean

--- backend/samenwerkfunctionaliteit/src/main/kotlin/com/ritense/valtimoplugins/samenwerkfunctionaliteit/autoconfigure/SamenwerkfunctionaliteitConfiguration.kt
+++ backend/samenwerkfunctionaliteit/src/main/kotlin/com/ritense/valtimoplugins/samenwerkfunctionaliteit/autoconfigure/SamenwerkfunctionaliteitConfiguration.kt
@@ -1,17 +1,8 @@
 package com.ritense.valtimoplugins.samenwerkfunctionaliteit.autoconfigure
 
-import com.ritense.valtimoplugins.samenwerkfunctionaliteit.client.DefaultSamenwerkfunctionaliteitClient
 import org.springframework.boot.context.properties.EnableConfigurationProperties
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.web.client.RestClient
 
 @Configuration
 @EnableConfigurationProperties
-class SamenwerkfunctionaliteitConfiguration {
-    @Bean
-    fun samenwerkfunctionaliteitPluginClient(restClientBuilder: RestClient.Builder): DefaultSamenwerkfunctionaliteitClient =
-        DefaultSamenwerkfunctionaliteitClient(
-            restClientBuilder,
-        )
-}
+class SamenwerkfunctionaliteitConfiguration
